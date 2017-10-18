6f7990ef6358458e7da48b92ecb7579af1ed717b
Fionar <466007639@qq.com>
2017-10-18 20:08:22 +0800

    'sendEmail'

--- store/src/impl/UserServiceImpl.java
+++ store/src/impl/UserServiceImpl.java
@@ -3,11 +3,15 @@ package impl;
 import dao.UserDao;
 import entity.User;
 import service.UserService;
+import utils.MailUtils;
 
 public class UserServiceImpl implements UserService {
     @Override
     public void regist(User user) throws Exception {
-        UserDao dao=new UserDaoImpl();
+        UserDao dao = new UserDaoImpl();
         dao.add(user);
+        String emailMsg = "注册成功";
+//                "<a href='http://localhost:8080/user?method=active&code=\"+user.getCode()+\"'>点此激活</a>";
+        MailUtils.sendMail(user.getEmail(), emailMsg);
     }
 }
--- store/src/utils/MailUtils.java
+++ store/src/utils/MailUtils.java
@@ -1,5 +1,8 @@
 package utils;
 
+import com.sun.mail.util.MailSSLSocketFactory;
+
+import java.security.GeneralSecurityException;
 import java.util.Properties;
 
 import javax.mail.Authenticator;
@@ -16,22 +19,24 @@ import javax.mail.internet.MimeMessage.RecipientType;
 public class MailUtils {
 
 	public static void sendMail(String email, String emailMsg)
-			throws AddressException, MessagingException {
+			throws AddressException, MessagingException, GeneralSecurityException {
 		// 1.创建一个程序与邮件服务器会话对象 Session
 
 		Properties props = new Properties();
 		//设置发送的协议
 		props.setProperty("mail.transport.protocol", "SMTP");
-		
-		//设置发送邮件的服务器
-		props.setProperty("mail.host", "smtp.126.com");
 		props.setProperty("mail.smtp.auth", "true");// 指定验证为true
-
+		//设置发送邮件的服务器
+		props.setProperty("mail.host", "smtp.qq.com");
+		MailSSLSocketFactory sf= new MailSSLSocketFactory();
+		sf.setTrustAllHosts(true);
+		props.put("mail.smtp.ssl.enable","true");
+		props.put("mail.smtp.ssl.socketFactory",sf);
 		// 创建验证器
 		Authenticator auth = new Authenticator() {
 			public PasswordAuthentication getPasswordAuthentication() {
 				//设置发送人的帐号和密码
-				return new PasswordAuthentication("sunquan_itcast", "sunquan@itcast");
+				return new PasswordAuthentication("466007639@qq.com", "ktetoogcsomscbeb");
 			}
 		};
 
@@ -41,10 +46,10 @@ public class MailUtils {
 		Message message = new MimeMessage(session);
 
 		//设置发送者
-		message.setFrom(new InternetAddress("sunquan_itcast@126.com"));
+		message.setFrom(new InternetAddress("466007639@qq.com"));
 
 		//设置发送方式与接收者
-		message.setRecipient(RecipientType.TO, new InternetAddress(email)); 
+		message.setRecipient(RecipientType.TO, new InternetAddress(email));
 
 		//设置邮件主题
 		message.setSubject("用户激活");
@@ -55,5 +60,6 @@ public class MailUtils {
 
 		// 3.创建 Transport用于将邮件发送
 		Transport.send(message);
+
 	}
 }
